4d9213bdf45b8dad5232ebbcbbf6b7e72c4f7249
Lennart Hansen <lennart.hansen@me.com>
2012-03-16 11:38:25 +0100

    first commit

---
+++ XBMC-Statusbar/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  XBMC-Statusbar
+//
+//  Created by Lennart Hansen on 16.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ XBMC-Statusbar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XBMC-Statusbar
+//
+//  Created by Lennart Hansen on 16.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
