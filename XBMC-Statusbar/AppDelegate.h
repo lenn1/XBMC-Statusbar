85d9416a5b8c8bccf79bca33c9231c23382a3fce
Lennart Hansen <lennart.hansen@me.com>
2012-03-16 16:51:43 +0100

    Projektanfang - Probleme mit der IP Adresse/Hostname - Preferences Window angefangen

--- XBMC-Statusbar/AppDelegate.h
+++ XBMC-Statusbar/AppDelegate.h
@@ -8,8 +8,28 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate,NSMenuDelegate>
+{
+    IBOutlet NSMenu *statusMenu;
+    NSStatusItem * statusItem;
+    NSString* username;
+    NSString* password;
+    NSString* ipAddress;
+    int port;
+    IBOutlet NSWindow* preferencesWindow;
+}
+- (IBAction)button_UpdateLibrary:(id)sender;
+- (IBAction)button_CleanLibrary:(id)sender;
+- (IBAction)button_WakeOnLan:(id)sender;
+- (IBAction)button_Suspend:(id)sender;
+- (IBAction)button_ShutDown:(id)sender;
+- (IBAction)button_Preferences:(id)sender;
+- (IBAction)button_Quit:(id)sender;
 
-@property (assign) IBOutlet NSWindow *window;
+@property(assign,nonatomic)NSString* ipAddress;
+@property(assign,nonatomic)NSString* username;
+@property(assign,nonatomic)NSString* password;
+@property int port;
+
 
 @end
--- XBMC-Statusbar/AppDelegate.m
+++ XBMC-Statusbar/AppDelegate.m
@@ -9,17 +9,77 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
-
-@synthesize window = _window;
-
+@synthesize username,password,ipAddress,port;
 - (void)dealloc
 {
     [super dealloc];
 }
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+-(void)awakeFromNib
 {
-    // Insert code here to initialize your application
+    
+    statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain];
+    [statusItem setMenu:statusMenu];
+    [statusItem setImage:[NSImage imageNamed:@"xbmc.png"]];
+    [statusItem setAlternateImage:[NSImage imageNamed:@"xbmc-inv.png"]];
+    [statusItem setHighlightMode:YES];
+    [statusItem setAction:@selector(updateFreeSpaceLabels)];
+    
+    statusMenu.delegate = self;
+    
+    //DEBUG
+    
+    self.ipAddress = @"xbmc.local";
+    self.username = @"xbmc";
+    self.password = @"hb.m,4dc";
+    self.port = 8080;
+    
 }
 
+- (IBAction)button_UpdateLibrary:(id)sender 
+{
+    
+    NSString* Url = [NSString stringWithFormat:@"http://%@:%@@%@:%d/xbmcCmds/xbmcHttp?command=ExecBuiltIn&parameter=XBMC.UpdateLibrary(video)",username,password,ipAddress,port];
+    NSURLRequest *myRequest = [[NSURLRequest alloc]initWithURL:[[NSURL alloc]initWithString:Url]];
+    [[[NSURLConnection alloc] initWithRequest:myRequest delegate:self] autorelease];
+
+}
+
+- (IBAction)button_CleanLibrary:(id)sender 
+{
+    NSString* Url = [NSString stringWithFormat:@"http://%@:%@@%@:%d/xbmcCmds/xbmcHttp?command=ExecBuiltIn&parameter=XBMC.CleanLibrary(video)",username,password,ipAddress,port];
+    NSURLRequest *myRequest = [[NSURLRequest alloc]initWithURL:[[NSURL alloc]initWithString:Url]];
+    [[[NSURLConnection alloc] initWithRequest:myRequest delegate:self] autorelease];
+}
+
+- (IBAction)button_WakeOnLan:(id)sender 
+{
+    
+}
+
+- (IBAction)button_Suspend:(id)sender 
+{
+    
+    NSString* Url = [NSString stringWithFormat:@"http://%@:%@@%@:%d/xbmcCmds/xbmcHttp?command=ExecBuiltIn&parameter=XBMC.Suspend",username,password,ipAddress,port];
+    NSURLRequest *myRequest = [[NSURLRequest alloc]initWithURL:[[NSURL alloc]initWithString:Url]];
+    [[[NSURLConnection alloc] initWithRequest:myRequest delegate:self] autorelease];
+
+}
+
+- (IBAction)button_ShutDown:(id)sender 
+{
+    NSString* Url = [NSString stringWithFormat:@"http://%@:%@@%@:%d/xbmcCmds/xbmcHttp?command=ExecBuiltIn&parameter=XBMC.Shutdown",username,password,ipAddress,port];
+    NSURLRequest *myRequest = [[NSURLRequest alloc]initWithURL:[[NSURL alloc]initWithString:Url]];
+    [[[NSURLConnection alloc] initWithRequest:myRequest delegate:self] autorelease];
+
+}
+
+- (IBAction)button_Preferences:(id)sender 
+{
+    [preferencesWindow makeKeyAndOrderFront:nil];
+}
+
+- (IBAction)button_Quit:(id)sender 
+{    
+    [NSApp terminate: nil]; 
+}
 @end
